ac083a3914fbab5da43bc01c8104f5d657786a9d
Misaki Koonce <misaki.koonce@gmail.com>
2019-08-23 12:43:43 -0700

    Feat: Change input type to number and display it in #results.

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,15 @@
 let results = [];
 // function takes userInput and add condition
 const outputNumbs = userInput => {
-    if(userInput !== Number){
-        alert("Please input integers!");
-    }
     for(let i = 0; i <= userInput; i++){
         if (i.toString().charAt(0) === "1") {
-            results.push("Beep!");
+            results.push(` "Beep!"`);
         } else if (i.toString().charAt(0) === "2") {
-            results.push("Boop!");
+            results.push(` "Boop!"`);
         } else if (i.toString().charAt(0) === "3") {
-            results.push("I'm sorry Dave, I'm afraid I can't do that!");
+            results.push(` "I'm sorry Dave, I'm afraid I can't do that!"`);
         } else {
-            results.push(i);
+            results.push(` ${i}`);
         }
     }
     return results;
@@ -25,8 +22,8 @@ $(document).ready(function(){
         event.preventDefault();
         let userInput = parseInt($("input").val());
         let showResult = outputNumbs(userInput);
-
         console.log("Result", showResult);
         $(".well").show();
+        $("#result").text(results);
     });
 });
